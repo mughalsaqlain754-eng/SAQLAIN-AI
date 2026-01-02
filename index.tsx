9cd68ca4df3fbc3aba08bca725b293fa64df2c76
mughalsaqlain754-eng <mughalsaqlain754@gmail.com>
2026-01-02 21:47:46 +0500

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+// 1. IMPORT HASHROUTER
+import { HashRouter } from 'react-router-dom'
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    {/* 2. WRAP YOUR APP IN HASHROUTER INSTEAD OF BROWSERROUTER */}
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
+)
